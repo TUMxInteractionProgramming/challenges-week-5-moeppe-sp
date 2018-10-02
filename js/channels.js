2531963f97734a6c8876c5c9398bab39ea191929
Maximilian Speicher <maximilian.speicher@tum.de>
2018-10-02 14:02:46 +0200

    Challenge 10 commit

--- js/channels.js
+++ js/channels.js
@@ -1,44 +1,51 @@
 var Yummy = {
     name: 'Yummy',
-    createdOn: new Date('April 1, 2016'),
+    createdOn: new Date(2016, 03, 01),
     createdBy: 'minus.plus.yummy',
     starred: false,
     expiresIn: 100,
     messageCount: 999,
+    messages: [],
 };
 
 var SevenContinents = {
     name: 'SevenContinents',
-    createdOn: new Date('April 1, 2016'),
-    createdBy: 'minus.plus.yummy',
+    createdOn: new Date(2016, 03, 02),
+    createdBy: 'cheeses.yard.applies',
     starred: true,
     expiresIn: 60,
     messageCount: 5,
+    messages: [],
 };
 
 var KillerApp = {
     name: 'KillerApp',
-    createdOn: new Date('April 1, 2016'),
-    createdBy: 'minus.plus.yummy',
+    createdOn: new Date(2016, 08, 30),
+    createdBy: 'lodge.bits.fake',
     starred: false,
     expiresIn: 1,
     messageCount: 10351,
+    messages: [],
 };
 
 var FirstPersonOnMars = {
     name: 'FirstPersonOnMars',
-    createdOn: new Date('April 1, 2016'),
-    createdBy: 'minus.plus.yummy',
+    createdOn: new Date(2016, 08, 28),
+    createdBy: 'snipped.atom.grid',
     starred: true,
     expiresIn: 30003,
     messageCount: 2424,
+    messages: [],
 };
 
 var Octoberfest = {
     name: 'Octoberfest',
-    createdOn: new Date('April 1, 2016'),
-    createdBy: 'minus.plus.yummy',
+    createdOn: new Date(2016, 08, 25),
+    createdBy: 'vocally.clearly.crawled',
     starred: false,
     expiresIn: 60,
     messageCount: 321,
+    messages: [],
 };
+
+var channels = [Yummy, SevenContinents, KillerApp, FirstPersonOnMars, Octoberfest];
--- js/script.js
+++ js/script.js
@@ -8,12 +8,18 @@ var currentLocation = {
     what3words: 'schwellen.zeit.lippe'
 };
 var currentMessage;
-
+var emojis = require('emojis-list');
+var emojisLoaded = false;
+var currentCriterion = 'new';
 /**
  * #6 #Switcher function for the #channels name in the right app bar
  * @param channelName Text which is set
  */
 function switchChannel(channelObject) {
+    abortChannelCreation();
+    for (var index in channelObject.messages){
+      $('#messages').append(createMessageElement(channelObject.messages[index]));
+    }
     currentChannel = channelObject;
     //Log the channel switch
     console.log("Tuning in to channel", channelObject);
@@ -34,7 +40,6 @@ function switchChannel(channelObject) {
        This is inefficient (jQuery has to search all channel list items), but we'll change it later on */
     $('#channels li').removeClass('selected');
     $('#channels li:contains(' + channelObject.name + ')').addClass('selected');
-
 
 }
 
@@ -67,6 +72,12 @@ function selectTab(tabId) {
  */
 function toggleEmojis() {
     /* $('#emojis').show(); // #show */
+    if (!emojisLoaded) {
+        for (var index in emojis) {
+            $('#emojis').append(emojis[index]);
+        }
+        emojisLoaded = true;
+    }
     $('#emojis').toggle(); // #toggle
 }
 
@@ -82,10 +93,14 @@ function Message(text) {
 
 function sendMessage() {
     var message = new Message($('#message-input').val());
-    if (message.text==''){
-      alert('Please enter a message before sending!');
+    if (message.text.length == 0) {
+        alert('Please enter a message before sending!');
+        return;
     }
     currentMessage = message;
+    currentChannel.messages.push(message);
+    currentChannel.messageCount += 1;
+    listChannels(currentCriterion);
     console.log(message);
     $('#messages').append(createMessageElement(message));
     $('#messages').scrollTop($('#messages')[0].scrollHeight);
@@ -103,12 +118,29 @@ function createMessageElement(messageObject) {
         '<em>' + expiresIn + ' min. left</em></h3><p style="box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.12);">' + messageObject.text + '</p><button class="accent">+5 min.</button><div>';
 }
 
-function listChannels() {
-    createChannelElement(Yummy);
-    createChannelElement(SevenContinents);
-    createChannelElement(KillerApp);
-    createChannelElement(FirstPersonOnMars);
-    createChannelElement(Octoberfest);
+function listChannels(crtiterion) {
+    currentCriterion = crtiterion;
+    $('#channel-list').empty();
+    switch (crtiterion) {
+        case 'new':
+            channels.sort(compareNew);
+            break;
+        case 'trending':
+            channels.sort(compareTrending);
+            break;
+        case 'favorites':
+            channels.sort(compareFavorites);
+            break;
+        case 'none':
+            break;
+        default:
+            channels.sort(compareNew);
+            break;
+    }
+
+    for (var ch in channels) {
+        createChannelElement(channels[ch]);
+    }
 
 }
 
@@ -117,7 +149,7 @@ function createChannelElement(channelObject) {
     var starClasses;
     starClasses = channelObject.starred ? 'fas fa-star' : 'far fa-star';
     var liAttr;
-    if (channelObject.name == 'SevenContinents') {
+    if (channelObject.name == currentChannel.name) {
         liAttr = {
             'onclick': 'switchChannel(' + channelObject.name + ')',
             'class': 'selected'
@@ -143,4 +175,83 @@ function createChannelElement(channelObject) {
 
     $('<li>').attr(liAttr).html('#' + channelObject.name).append($('<span>').attr('class', 'channel-meta').css(parentBoxesStyle).append($('<i>').attr('class', starClasses)).append($('<span>' + channelObject.expiresIn + ' min</span>')
         .css(boxesStyle)).append($('<span>' + channelObject.messageCount + ' new</span>').css(boxesStyle)).append($('<i>').attr('class', 'fas fa-chevron-right'))).appendTo('#channel-list');
+}
+
+function compareNew(a, b) {
+    return b.createdOn.getTime() - a.createdOn.getTime();
+}
+
+function compareTrending(a, b) {
+    return b.messageCount - a.messageCount;
+}
+
+function compareFavorites(a, b) {
+    if (a.starred) {
+        if (!b.starred) {
+            return -1;
+        } else {
+            return b.createdOn.getTime() - a.createdOn.getTime();
+        }
+    } else {
+        if (b.starred) {
+            return 1;
+        } else {
+            return b.createdOn.getTime() - a.createdOn.getTime();
+        }
+    }
+}
+
+function fabClicked() {
+    console.log("Hello");
+    $('#messages').empty();
+    $('#add-channel-bar').toggle();
+    $('#current-channel-bar').toggle();
+    $('#create-channel-button').toggle();
+    $('#send-message').toggle();
+}
+
+function abortChannelCreation() {
+    $('#messages').empty();
+    $('#add-channel-bar').toggle();
+    $('#current-channel-bar').toggle();
+    $('#create-channel-button').toggle();
+    $('#send-message').toggle();
+    $('#message-input').val('');
+}
+
+function createNewChannel() {
+    var message = new Message($('#message-input').val());
+    var channelName = $('#new-channel-name').val();
+
+    if (channelName.length != 0) {
+        if (channelName.charAt(0) == '#') {
+            if (!/\s/.test(channelName)) {
+                if (message.text.length != 0) {
+                    var channel = new Channel(channelName, message);
+                    channels.push(channel);
+                    listChannels(currentCriterion);
+                    switchChannel(channel);
+                } else {
+                    alert("Please enter a message!");
+                }
+            } else {
+                alert('Channel name must not contain any whitespaces!');
+            }
+        } else {
+            alert('Channel name needs to start witch "#"!');
+        }
+    } else {
+        alert('Please enter a channel name!');
+    }
+}
+
+function Channel(channelName, message) {
+    channelName = channelName.substr(1);
+    this.name = channelName;
+    this.createdOn = new Date(Date.now());
+    this.createdBy = currentLocation.what3words;
+    this.starred = false;
+    this.expiresIn = 60;
+    this.messageCount = 1;
+    this.messages = [message];
 }
